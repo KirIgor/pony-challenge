1815710f2b6f72649c8aa5e78ec40071649ee1f7
igor <ig6966.ivanov@gmail.com>
2019-04-08 19:04:33 +0600

    new game page mockup

--- src/pages/new_game.tsx
+++ src/pages/new_game.tsx
@@ -1,18 +1,39 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import * as Routes from '../constants/routes.json';
+// import { Link } from 'react-router-dom';
+// import * as Routes from '../constants/routes.json';
 
-import './hello.css';
+import { PonyName } from '../types/index';
+import { range } from '../utils/helper';
+
+import './new_game.css';
 
 interface Props {}
 
-const Hello = React.memo(({  }: Props) => {
+const renderSizeSelect = () => {
+	return <select>{range(11).map(i => <option>{i + 15}</option>)}</select>;
+};
+
+const renderPonyNameSelect = () => {
+	return <select>{Object.values(PonyName).map(v => <option>{v}</option>)}</select>;
+};
+
+const renderDifficultySelect = () => {
+	return <select>{range(11).map(i => <option>{i}</option>)}</select>;
+};
+
+const NewGame = React.memo(({  }: Props) => {
 	return (
-		<div className="hello-container">
-			Hello, %username%!
-			<Link to={Routes.NEW_GAME}>Play</Link>
+		<div className="new-game-container">
+			<div className="new-game-items">
+				<div>
+					{renderSizeSelect()}X{renderSizeSelect()}
+				</div>
+				<div>{renderPonyNameSelect()}</div>
+				<div>difficulty: {renderDifficultySelect()}</div>
+				<button>New game</button>
+			</div>
 		</div>
 	);
 });
 
-export default Hello;
+export default NewGame;
